ec8e0d77a4acfe746161fa1da2afee01e78c128a
esadv6 <esadv6@DESKTOP-91EI809>
2017-03-16 10:05:24 -0300

    UC01CadastrarEmpresa - versao inicial

--- sceweb/src/br/sceweb/controle/ServletControle.java
+++ sceweb/src/br/sceweb/controle/ServletControle.java
@@ -16,11 +16,7 @@ import br.sceweb.modelo.EmpresaDAO;
  */
 public class ServletControle extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	Logger logger = Logger.getLogger(ServletControle.class);
-	String mensagem = "";
-	EmpresaDAO empresaDAO;
-	String cnpjParaExclusao = "";// seta o cnpj para exclusao
-
+	
 	/**
 	 * Default constructor.
 	 */
@@ -44,102 +40,8 @@ public class ServletControle extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.setAttribute("erro", null);
-		executaComando(request, response);
+		
 	}
 
-	protected void executaComando(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String parametro = request.getParameter("acao");
-		logger.info("acao do servletcontrole = " + parametro);
-		String url = "";
-		String resultado = "";
-		request.setAttribute("erro", null);
-		if (parametro.equals("IncluirEmpresa")) {
-			url = "/visao/FormEmpresa.jsp";
-			try {
-				resultado = cadastrarEmpresa(request.getParameter("txtCNPJ"), request.getParameter("txtNomeDaEmpresa"),
-						request.getParameter("txtNomeFantasia"), request.getParameter("txtEndereco"),
-						request.getParameter("txtTelefone"));
-				logger.info("resultado do cadastra = " + resultado);
-				request.setAttribute("msg", resultado);
-				request.getRequestDispatcher(url).forward(request, response);
-			} catch (Exception e) {
-				request.setAttribute("msg", resultado);
-				request.getRequestDispatcher(url).forward(request, response);
-				logger.info("erro  = " + e.getMessage());
-
-			}
-		}
-		if (parametro.equals("ConsultarEmpresa")) {
-			url = "/visao/FormEmpresa.jsp";
-			Empresa empresa = new Empresa();
-			String cnpj = request.getParameter("txtCNPJ");
-			logger.info("consulta empresa  = " + cnpj);
-			try {
-				if (!cnpj.isEmpty()) {
-					empresa = consulta(cnpj);
-					if (empresa != null){
-						logger.info("consulta empresa nome da empresa  = " + empresa.getNomeDaEmpresa());
-						request.setAttribute("nomeDaEmpresa", empresa.getNomeDaEmpresa());
-					    request.setAttribute("cnpj", empresa.getCnpj());
-					    request.setAttribute("nomeFantasia", empresa.getNomeFantasia());
-					    request.setAttribute("endereco", empresa.getEndereco());
-					    request.setAttribute("telefone", empresa.getTelefone());
-					    request.setAttribute("msg", "");
-						url = "/visao/FormEmpresaResultadoDaConsulta.jsp";
-					} else {
-						request.setAttribute("msg", "cnpj invalido");
-					}
-				} else {
-					request.setAttribute("msg", "cnpj invalido");
-				}
-			} catch (Exception e) {
-				logger.info(e.getMessage() + e.getCause());
-			}
-			request.getRequestDispatcher(url).forward(request, response);
-			
-		}
-
-	}
-
-	public String cadastrarEmpresa(String cnpj, String nomeDaEmpresa, String nomeFantasia, String endereco,
-			String telefone) {
-		String msg = "";
-		Empresa empresa = new Empresa();
-		EmpresaDAO empresaDAO = new EmpresaDAO();
-		try {
-			empresa.setCnpj(cnpj);
-			empresa.setNomeDaEmpresa(nomeDaEmpresa);
-			empresa.setNomeFantasia(nomeFantasia);
-			empresa.setEndereco(endereco);
-			empresa.setTelefone(telefone);
-			empresaDAO.adiciona(empresa);
-			msg = "cadastro realizado com sucesso";
-
-		} catch (Exception e) {
-			msg = e.getMessage();
-		}
-
-		return msg;
-	}
-
-	public Empresa consulta(String cnpj) {
-		logger.info("consulta empresa 2 = " + cnpj);
-		EmpresaDAO empresaDAO = new EmpresaDAO();
-		return empresaDAO.consultaEmpresa(cnpj);
-	}
-
-	public String excluirEmpresa(String cnpj) {
-		String msg = "";
-		EmpresaDAO empresaDAO = new EmpresaDAO();
-		try {
-			empresaDAO.exclui(cnpj);
-			msg = "excluido com sucesso";
-		} catch (Exception e) {
-			msg = e.getMessage();
-		}
-
-		return msg;
-	}
+	
 }
--- sceweb/src/br/sceweb/modelo/EmpresaDAO.java
+++ sceweb/src/br/sceweb/modelo/EmpresaDAO.java
@@ -13,68 +13,10 @@ import br.sceweb.servico.FabricaDeConexoes;
 public class EmpresaDAO {
 	
 	public int adiciona(Empresa empresa){
-		PreparedStatement ps;
-		int codigoRetorno=0;
-		try (Connection conn = new FabricaDeConexoes().getConnection()){
-			ps = (PreparedStatement) conn.prepareStatement(
-					"insert into empresa (cnpj, nomeDaEmpresa, nomeFantasia, endereco, telefone) values(?,?,?,?,?)");
-			ps.setString(1,empresa.getCnpj());
-			ps.setString(2, empresa.getNomeDaEmpresa());
-			ps.setString(3, empresa.getNomeFantasia());
-			ps.setString(4, empresa.getEndereco());
-			ps.setString(5, empresa.getTelefone());
-			codigoRetorno = ps.executeUpdate();
-			ps.close();
-			
-		} catch (SQLException e){
-				throw new RuntimeException(e);
-			}
-		return codigoRetorno;
-	}
-	/**
-	 * exclui uma empresa pelo cnpj
-	 * @param cnpj
-	 * @return 0 erro na exclusao ou 1 excluido com sucesso
-	 */
-	public int exclui (String cnpj) {
-		java.sql.PreparedStatement ps;
-		int codigoretorno = 0;
-		try (Connection conn = new FabricaDeConexoes().getConnection()) {
-			ps= conn.prepareStatement ("delete from empresa where cnpj = ?");
-			ps.setString(1, cnpj);
-			codigoretorno = ps.executeUpdate();
-			}
-		catch (SQLException e){
-			throw new RuntimeException(e);
-		}
-	return codigoretorno;
-	
-	}
-	
-	public static Empresa consultaEmpresa(String cnpj) {
-		Empresa empresa = null;
-		java.sql.PreparedStatement ps;
-		try (Connection conn = new FabricaDeConexoes().getConnection()) {
-			ps = conn.prepareStatement("select * from empresa where cnpj = ?");
-			ps.setString(1, cnpj);
-			ResultSet resultSet = ps.executeQuery();
-			while (resultSet.next()) {
-				empresa = new Empresa();
-				empresa.setCnpj(resultSet.getString("cnpj"));
-				empresa.setNomeDaEmpresa(resultSet.getString("nomeDaEmpresa"));
-				empresa.setNomeFantasia(resultSet.getString("nomeFantasia"));
-				empresa.setEndereco(resultSet.getString("endereco"));
-				empresa.setTelefone(resultSet.getString("telefone"));
-				
-			}
-			resultSet.close();
-			ps.close();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
 		
-		return empresa;
+		return 1;
 	}
+
 }
 
 
